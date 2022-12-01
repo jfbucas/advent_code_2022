46beec7804812caf85dc89fad7173798e7208e05
Edo <edo@waargla>
2022-12-01 15:14:27 +0000

    0xF00D take #2

--- Day1/food.sh
+++ Day1/food.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# Part 1
 awk 'BEGIN{ max=0; total =0; } /^$/ { if (total > max) { max = total;}; total = 0 } /[0-9]*/ { total += ; } END{ print max };' food.txt 
+
+# Part 2
+echo $(($(awk 'BEGIN{ max=0; total =0; } /^$/ { print total; total = 0 } /[0-9]*/ { total += $1; };' food.txt | sort -n | tail -3 | tr "\n" "+")"0"))
