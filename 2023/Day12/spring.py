2e218b05e8f0d7da3ae7d979c53141908b6e5bf4
Jef Bucas <jfbucas@stp.dias.ie>
2023-12-13 15:27:51 +0000

    Day12 part2 almost there

--- 2023/Day12/spring.py
+++ 2023/Day12/spring.py
@@ -34,7 +34,7 @@ def get_str_combinations(input_str):
 	_get_combinations((), input_str)
 	return combinations
 
-def compare(totest, desc):
+def compare_to_desc(totest, desc):
 	if len(totest) != len(desc):
 		return None
 
@@ -50,7 +50,7 @@ def get_variations(row):
 
 	desc=row["desc"]
 
-	print(row)
+	#print(row)
 	minimum = []
 	for x in row["faulties"][:-1]:
 		minimum.append("#"*x)
@@ -58,14 +58,14 @@ def get_variations(row):
 	minimum.append("#"*row["faulties"][-1])
 
 	minimum_str = "".join(minimum)
-	#print(minimum, minimum_str)
+	#print("Min:", minimum, minimum_str)
 
 	if len(minimum_str) > len(desc):
-		return 0
+		return { desc:None }
 
 	missing_count = len(desc) - len(minimum_str)
 	if missing_count == 0:
-		return 0 if compare(minimum_str, desc) == None else 1
+		return { desc: [ compare_to_desc(minimum_str, desc) ] }
 	else:
 		#print("We need to insert ", missing_count, "springs into", minimum_str)
 		missing_springs = get_str_combinations("."*missing_count)
@@ -82,29 +82,66 @@ def get_variations(row):
 					tmp.insert(rpos[i], missing[i])
 
 				#print(tmp)
-				comp = compare("".join(tmp), desc)
+				comp = compare_to_desc("".join(tmp), desc)
 				if comp != None:
 					if comp not in accepted:
 						accepted.append(comp)
 				#print(pos, "".join(tmp), desc, len(accepted))
 
-		return len(list(set(accepted)))
+		return { desc: accepted }
 
+total = {}
+for s in springs:
+	v = get_variations(s)
 
-#total = 0
-#for s in springs:
-#	total += get_variations(s)
-#
-#print(total)
+	for k in v.keys():
+		if v[k] != None:
+			total[k]= v[k]
+
+total = [ y for x in total.values() for y in x ]
+print(len(total))
+
 
 # Part 2
 
 unfolded_springs = []
 for s in springs:
-	unfolded_springs.append({"desc":"?".join([s["desc"]]*5), "faulties":s["faulties"]*5})
+	#unfolded_springs.append({"desc":"?".join([s["desc"]]*5), "faulties":s["faulties"]*5})
+	unfolded_springs.append({"desc":s["desc"]+"?", "faulties":s["faulties"]})
 
 total = 0
-for s in unfolded_springs:
-	total += get_variations(s)
+for s in springs:
+	a = s["desc"]
+	b = s["desc"]+"?"
+	c = "?"+s["desc"]+"?"
+	d = "?"+s["desc"]
+
+	sa = { "desc":a, "faulties":s["faulties"] }
+	sb = { "desc":b, "faulties":s["faulties"] }
+	sc = { "desc":c, "faulties":s["faulties"] }
+	sd = { "desc":d, "faulties":s["faulties"] }
+
+	va = get_variations(sa)
+	vb = get_variations(sb)
+	vc = get_variations(sc)
+	vd = get_variations(sd)
+
+	ca = len(va[a])
+	cb = len(vb[b])
+	cc = len(vc[c])
+	cd = len(vd[d])
+
+	best_count = 0
+	unfolded_str = "?".join([s["desc"]]*5)
+	for comb in itertools.product('abcd', repeat=5):
+		exec("comb_str = "+comb[0]+"+"+comb[1]+"+"+comb[2]+"+"+comb[3]+"+"+comb[4])
+		if comb_str == unfolded_str:
+			exec("comb_count = c"+comb[0]+"*c"+comb[1]+"*c"+comb[2]+"*c"+comb[3]+"*c"+comb[4])
+			if best_count < comb_count:
+				best_count = comb_count
+				print(comb_count)
+
+	total+=best_count
+	print(a, best_count)
 
 print(total)
