f5fe7b0fc31cd9df3a0a91a152f6cc2f7a06d9a8
Jef Bucas <jfbucas@stp.dias.ie>
2024-12-17 10:59:39 +0000

    Day16

--- 2024/Day16/day.py
+++ 2024/Day16/day.py
@@ -8,7 +8,11 @@ import random
 
 sys.setrecursionlimit(150000)
 
+MAX=100000000000
+
 room=[]
+room_score={}
+best_seats={}
 start=(0,0)
 finish=(0,0)
 robot=(0,0,0)
@@ -29,55 +33,57 @@ for line in f.readlines():
 	room.append(list(line.replace("S",".").replace("E",".")))
 	y+=1
 
+# Define room infinity
+y=0
+for r in room:
+	x=0
+	for s in r:
+		for d in [0,1,2,3]:
+			room_score[ (x,y,d) ] = MAX
+		x+=1
+	y+=1
+
 def dist_to_finish(p):
 	px, py, pd = p
 	return (finish[0]-px)*(finish[0]-px) + (finish[1]-py)+(finish[1]-py)
 
 
-def find_path(pos, score, max_turn):
+def find_path(pos, score):
+	room_score[ pos ] = score
 	curx, cury, curd = pos
+
 	if (curx,cury) == finish:
-		return score
-	else:
-		#if max_turn == 0:
-		#	print(".", end="")
-			#print("-----")
-			#for r in room:
-			#	print("".join(r).replace("#", " "))
+		if score not in best_seats.keys():
+			best_seats[score]=[]
+			for r in room:
+				tmp = []
+				for c in r:
+					tmp.append(" ")
+				best_seats[score].append(tmp)
+		y=0
+		for r in room:
+			x=0
+			for c in r:
+				if c == "x":
+					best_seats[score][y][x]=c
+				x+=1
+			y+=1
 
-		best_score = 10000000000
+	# Try to move straight
+	newx, newy = curx+steps[curd][0], cury+steps[curd][1]
+	if room[ newy ][ newx ] == "." and room_score[ (newx,newy,curd) ] >= score+1:
+		room[ newy ][ newx ] = "x"
+		find_path((newx, newy, curd), score+1)
+		room[ newy ][ newx ] = "."
 
-		# Try to move straight
-		newx, newy = curx+steps[curd][0], cury+steps[curd][1]
-		if room[ newy ][ newx ] == ".":
+	# Try to turn
+	next_step_options = []
+	for newd in turn[curd]:
+		newx, newy = curx+steps[newd][0], cury+steps[newd][1]
+		if room[ newy ][ newx ] == "." and room_score[ (newx,newy,newd) ] >= score+1001:
 			room[ newy ][ newx ] = "x"
-			tmp_score = find_path((newx, newy, curd), score+1, max_turn)
-			if tmp_score < best_score:
-				return tmp_score
-				best_score = tmp_score
+			find_path((newx, newy, newd), score+1001)
 			room[ newy ][ newx ] = "."
-
-		# Try to turn
-		if max_turn > 0:
-			next_step_options = []
-			for newd in turn[curd]:
-				newx, newy = curx+steps[newd][0], cury+steps[newd][1]
-				if room[ newy ][ newx ] == ".":
-					next_step_options.append((newx, newy, newd))
-
-			if len(next_step_options) > 0:
-
-				for newx, newy, newd in sorted(next_step_options, key=lambda x:-dist_to_finish(x)):
-						
-					room[ newy ][ newx ] = "x"
-					tmp_score = find_path((newx, newy, newd), score+1001, max_turn-1)
-					if tmp_score < best_score:
-						return tmp_score
-						best_score = tmp_score
-					room[ newy ][ newx ] = "."
-	
-		#print(best_score)
-		return best_score
 
 
 	
@@ -90,22 +96,16 @@ total=0
 # Start on S, facing East
 robot=(start[0],start[1],0)
 
-best_score = 10000000000
-#for mt in range(0,40):
-for mt in range(150,200):
-	print("Trying",mt,"turns")
-	tmp_score = find_path(robot, 0, mt)
-	if tmp_score < best_score:
-		best_score = tmp_score
-		break
-print(best_score)
-exit()
+room[ start[1] ][ start[0] ] = "x"
+find_path(robot, 0)
+room[ start[1] ][ start[0] ] = "."
+total = min([room_score[ (finish[0],finish[1],d) ] for d in [0,1,2,3]])
 print("Part1:", total)
 
 
 ############################
 # Part 2
 
-total=0
+total = "".join(["".join(r) for r in best_seats[total]]).count("x")
 print("Part2:", total)
 
